aec4543b36a0debd72e52df37ac28267eb7244d8
beminee <emirdota2@gmail.com>
2019-02-20 09:27:09 +0300

    Some refactoring and clean-ups

--- keygen.py
+++ keygen.py
@@ -14,23 +14,29 @@ def ripemd160(val):
     d.update(val)
     return d
 
-#generate private key
-random.seed(666) # generate a key with random.seed
-priv_key = bytes([random.randint(0, 255) for x in range(32)])
+def generate_key_with_seed(seed=666):
+    #generate private key
+    random.seed(seed) # generate a key with random.seed
+    priv_key = bytes([random.randint(0, 255) for x in range(32)])
 
-# priv_key => wallet_import_format
-full_key = b'\x80' + priv_key
-wallet_import_format = b58wchecksum(full_key)
+    # priv_key => wallet_import_format
+    wallet_import_format = b58wchecksum(b'\x80' + priv_key)
 
-# priv_key to public_key
-sk = ecdsa.SigningKey.from_string(priv_key, curve=ecdsa.SECP256k1)
-vk = sk.get_verifying_key()
-publ_key = b'\x04' + vk.to_string()
-hash160 = ripemd160(hashlib.sha256(publ_key).digest()).digest()
-publ_addr_a = b'\x00' + hash160
-publ_addr_b = b58wchecksum(publ_addr_a)
-# printing info
-# can be verified over https://coinb.in/#verify
-print("Private Key: {}".format(str(shex(priv_key))))
-print("Wallet Import Format (WIF_Priv_key): {}".format(str(wallet_import_format)))
-print("Wallet Address: {}".format(str(publ_addr_b)))
+    # priv_key to public_key
+    sk = ecdsa.SigningKey.from_string(priv_key, curve=ecdsa.SECP256k1)
+    vk = sk.get_verifying_key()
+    publ_key = b'\x04' + vk.to_string()
+    hash160 = ripemd160(hashlib.sha256(publ_key).digest()).digest()
+    publ_addr = b58wchecksum(b'\x00' + hash160)
+    # debug
+    # can be verified over https://coinb.in/#verify
+    # print("Private Key: {}".format(str(shex(priv_key))))
+    # print("Wallet Import Format (WIF_Priv_key): {}".format(str(wallet_import_format)))
+    # print("Wallet Address: {}".format(str(publ_addr_b)))
+    return priv_key, wallet_import_format, publ_addr
+
+
+priv_key, wallet_import_format, publ_addr = generate_key_with_seed(666)
+print("Private Key: {}".format(str(shex(priv_key)), "utf-8"))
+print("Wallet Import Format (WIF_Priv_key): {}".format(str(wallet_import_format, "utf-8")))
+print("Wallet Address: {}".format(str(publ_addr, "utf-8")))
